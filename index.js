831d2b46b1a146635a502fb49f88c1af34f596fa
bthesorceror <bthesorceror@gmail.com>
2013-08-25 21:06:53 -0400

    Extract cookie getting and setting to methods

--- index.js
+++ index.js
@@ -101,21 +101,26 @@ function CouchCushion(url, ttl) {
   this.ttl = ttl || 30 * 60 * 1000;
 }
 
+CouchCushion.prototype.getSessionId = function(req, res) {
+  return (new Cookies(req, res)).get('session_id');
+}
+
+CouchCushion.prototype.setSessionId = function(req, res, id) {
+  (new Cookies(req, res)).set('session_id', id);
+}
+
 CouchCushion.prototype.middleware = function() {
   var self = this;
   return function(req, res, next) {
-    var cookies = new Cookies(req, res);
-    var id = cookies.get('session_id');
     req.session = new SessionManager(self.connection, self.ttl);
 
     req.session.on('error', function(err) {
       this.handleError(req, res, err);
     }.bind(this));
 
-    req.session.setup(id, function() {
-      cookies.set('session_id', req.session.id);
-      next();
-    });
+    req.session.setup(this.getSessionId(req, res), function() {
+      this.setSessionId(req, res, req.session.id); next();
+    }.bind(this));
   }
 }
 
